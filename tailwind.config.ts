4e5227655e52e7a162d588d3d2d034b5f4bc6d2c
JethroCenas <87797179+Newiie@users.noreply.github.com>
2024-08-20 20:24:40 +0800

    Added Navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,26 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      backgroundColor: {
+        "primary": "#1b4a0aca",
+        "hover": "#44de108f",
+        "secondary-btn": "#a3e279",
+        "paleGreen": "#hsl(96, 75%, 89%)",
+        "white": "#FAFAFA",
+        "black": "#010101"
+      },
+      colors: {
+        "primary": "#1b4a0aca",
+        "hover": "#44de108f",
+        "paleGreen": "#hsl(96, 75%, 89%)",
+        "white": "#FAFAFA",
+        "black": "#010101",
+        "nav": "#333333"
+      },
+      fontSize: {
+        "header": "2.5rem",
+        "h-15": "1.5rem",
+      }
     },
   },
   plugins: [],
