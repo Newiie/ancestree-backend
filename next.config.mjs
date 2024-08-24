3a3a1601427e0ab0a5b8c318ad5434f92897f56e
JethroCenas <87797179+Newiie@users.noreply.github.com>
2024-08-24 11:18:29 +0800

    Added User Authentication

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Add rewrites for proxying API requests
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/api/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
